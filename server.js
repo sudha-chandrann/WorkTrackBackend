d9e9c616ddacb1c5983cfd8d5cf832fcd9f28766
sudhachandrann <sudhachandran1020@gmail.com>
2025-04-05 02:19:14 +0530

    add variable

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 // Socket.IO setup
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", 
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST"]
   }
 });
@@ -111,6 +111,6 @@ io.on("connection", async (socket) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+server.listen(PORT,  "0.0.0.0",() => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
